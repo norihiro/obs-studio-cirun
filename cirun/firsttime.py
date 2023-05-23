c21b3016a1edba17975625fa456d5e548ebac02c
Norihiro Kamae <norihiro@nagater.net>
2023-05-23 09:58:32 +0900

    cirun/firsttime.py: Click App Permission dialogs on macOS

--- cirun/firsttime.py
+++ cirun/firsttime.py
@@ -2,6 +2,7 @@
 
 import os
 import subprocess
+import sys
 import pyautogui
 from time import sleep
 from obsexec import OBSExec
@@ -11,6 +12,35 @@ import util
 
 u = util.u
 record = None
+
+def _location_width(t):
+    return t.location.x1 - t.location.x0
+
+def _app_permissions_macos():
+    util.take_screenshot()
+    util.wait_text('Review App Permissions', timeout=45)
+
+    t_camera_desc = u.find_text('This permission is needed in order to capture content from a webcam or capture card.')
+    t_microphone_desc = u.find_text('OBS requires this permission if you want to capture your microphone.')
+    t_hotkeys_desc = u.find_text('For keyboard shortcuts (hotkeys) to work while other apps are focused, please enable this permission.')
+    w_est = max(_location_width(t_camera_desc), _location_width(t_microphone_desc), _location_width(t_hotkeys_desc)) * 2
+
+    # Microphone
+    x0 = t_microphone_desc.location.x0
+    y0 = t_microphone_desc.location.y1
+    x1 = x0 + w_est
+    y1 = t_hotkeys_desc.location.y0
+    u.ocr(crop=(x0, y0, x1, y1))
+    util.click_verbose(u.find_text('Request Access'))
+    util.take_screenshot()
+    # FIXME: Untriseptium cannot find `OK` button. Crop left and right to make OCR success.
+    screen_size = pyautogui.size()
+    util.wait_text('OBS needs to access the microphone', timeout=6, ocrfunc=\
+            lambda u: u.ocr(crop=(screen_size.width/2-125, 0, screen_size.width/2+125, screen_size.height)) )
+    util.click_verbose(u.find_text('OK'))
+    util.take_screenshot()
+
+    util.click_verbose(u.find_text('Continue'))
 
 def run_firsttime():
     global obs, record
@@ -23,6 +53,10 @@ def run_firsttime():
 
     # Start OBS Studio
     obs = OBSExec(obsconfig.OBSConfigClean())
+
+    # App Permission
+    if sys.platform == 'darwin':
+        _app_permissions_macos()
 
     util.wait_text('Specify what you want to use the program for', timeout=5)
     util.take_screenshot()
