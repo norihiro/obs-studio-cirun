5c75036131b23a415ad8537af8976f615b52b4c1
Norihiro Kamae <norihiro@nagater.net>
2023-05-26 01:26:19 +0900

    Add unit test

---
+++ cirun/obstest.py
@@ -0,0 +1,33 @@
+import psutil
+import sys
+import unittest
+from obsexec import OBSExec
+from obsconfig import OBSConfigCopyFromSaved
+import util
+
+
+def _is_obs_running():
+    for p in psutil.process_iter():
+        if sys.platform == 'linux' and p.name() == 'obs':
+            return True
+        if sys.platform == 'darwin' and p.name() == 'OBS':
+            return True
+    return False
+
+
+class OBSTest(unittest.TestCase):
+    def setUp(self, config_name='obs-config-default'):
+        if _is_obs_running():
+            raise Exception('OBS has already been running')
+        self.obs = OBSExec(OBSConfigCopyFromSaved(config_name))
+        if sys.platform == 'linux':
+            util.wait_text('File Edit View Dock Profile Scene Collection Tools Help', timeout=10,
+                           ocrfunc=lambda u: util.ocr_topwindow(mode='top', length=200))
+        elif sys.platform == 'darwin':
+            util.wait_text('OBS Studio File Edit View Dock Profile Scene Collection Tools Help', timeout=10,
+                           ocrfunc=lambda u: u.ocr(crop=(0, 0, u.screenshot.width, 40)) )
+
+    def tearDown(self):
+        self.obs.term()
+        if _is_obs_running():
+            raise Exception('OBS did not exit')
---
+++ cirun/test_sources.py
@@ -0,0 +1,156 @@
+import os
+import random
+import sys
+from time import sleep
+import unittest
+import pyautogui
+import obstest
+import util
+
+
+sources = [
+        {
+            'inputName': 'background',
+            'sceneName': 'Background Scene',
+            'inputKind': 'color_source_v3',
+            'inputSettings': {
+                'color': 0xff582416,
+                },
+            },
+        {
+            'inputName': 'text',
+            'sceneName': 'Scene Text',
+            'inputKind': 'text_ft2_source_v2',
+            'inputSettings': {
+                'text': 'Text Source',
+                },
+            'update_settings': [
+                {
+                    'outline': True,
+                    },
+                {
+                    'outline': False,
+                    'drop_shadow': True,
+                    },
+                {
+                    'drop_shadow': False,
+                    },
+                ]
+            },
+        {
+            'inputName': 'images',
+            'sceneName': 'Scene Images',
+            'inputKind': 'slideshow',
+            'inputSettings': {
+                'files': [
+                    {
+                        'value': os.getcwd() + '/screenshot',
+                        'selected': False,
+                        'hidden': False,
+                        },
+                    ],
+                },
+            },
+        {
+            'inputName': 'browser',
+            'sceneName': 'Scene Browser',
+            'inputKind': 'browser_source',
+            'inputSettings': {
+                'url': 'https://www.youtube.com/watch?v=ghuB0ozPttI&list=UULFp_UXGmCCIRaX5pyFljICoQ',
+                'reroute_audio': False,
+                },
+            'sleep_after_creation': 5,
+            'update_settings': [
+                {
+                    'reroute_audio': True
+                    },
+                {
+                    'reroute_audio': False
+                    },
+                ]
+            },
+        {
+            'inputName': 'xshm',
+            'sceneName': 'Scene Desktop',
+            'inputKind': 'xshm_input',
+            'inputSettings': {
+                },
+            'skip': (sys.platform != 'linux')
+            },
+        {
+            'inputName': 'avcapture',
+            'sceneName': 'AV Capture Input',
+            'inputKind': 'av_capture_input',
+            'inputSettings': {
+                },
+            'skip': (sys.platform != 'darwin')
+            },
+        ]
+
+background_source = sources[0]['sceneName']
+
+
+def _mouse_move_around_preview():
+    geometry = util.current_window_geometry()
+    x = int(random.uniform(geometry[0] + 10, (geometry[0] * 2 + geometry[2] * 1) / 3))
+    y = int(random.uniform(geometry[1] + 50, (geometry[1] * 3 + geometry[3] * 2) / 5))
+    pyautogui.moveTo(x, y)
+
+
+class OBSSourceTest(obstest.OBSTest):
+    def test_add_remove_sources(self):
+        util.set_screenshot_prefix('screenshot/test_add_remove_sources-')
+        cl = self.obs.get_obsws()
+        cl.set_studio_mode_enabled(True)
+        input_kinds = cl.send('GetInputKindList').input_kinds
+
+        scenes = set()
+        for scene in cl.send('GetSceneList').scenes:
+            scenes.add(scene['sceneName'])
+
+        for source in sources:
+            if 'skip' in source and source['skip']:
+                continue
+            with self.subTest(inputKind=source['inputKind']):
+                sceneName = source['sceneName']
+                if not sceneName in scenes:
+                    cl.send('CreateScene', {'sceneName': sceneName})
+                    scenes.add(sceneName)
+                if sceneName != background_source:
+                    cl.send('CreateSceneItem', {'sceneName': sceneName, 'sourceName': background_source})
+                cl.send('CreateInput', source)
+                cl.send('SetCurrentPreviewScene', {'sceneName': sceneName})
+                cl.send('SetCurrentProgramScene', {'sceneName': sceneName})
+                if 'sleep_after_creation' in source:
+                    sleep(source['sleep_after_creation'])
+                util.take_screenshot()
+                if 'update_settings' in source:
+                    for settings in source['update_settings']:
+                        name = source['inputName']
+                        data = {'inputName': name, 'inputSettings': settings}
+                        print('SetInputSettings %s' % data)
+                        cl.send('SetInputSettings', data)
+                        if 'sleep_after_creation' in source:
+                            sleep(source['sleep_after_creation'])
+                        util.take_screenshot()
+                _mouse_move_around_preview()
+
+        with self.subTest(msg='exit and start again'):
+            self.assertTrue(obstest._is_obs_running())
+            self.obs.term()
+            self.assertFalse(obstest._is_obs_running())
+            self.obs.run()
+            self.assertTrue(obstest._is_obs_running())
+
+        with self.subTest(msg='cleanup'):
+            cl = self.obs.get_obsws()
+            for source in sources:
+                sceneName = source['sceneName']
+                if sceneName in scenes:
+                    cl.send('RemoveScene', {'sceneName': sceneName})
+                    scenes.remove(sceneName)
+            util.take_screenshot()
+
+
+if __name__ == '__main__':
+    unittest.main()
