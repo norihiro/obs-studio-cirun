e4b8d9dc0fe9d53a36a5c5737ebc96038a905f17
Norihiro Kamae <norihiro@nagater.net>
2023-05-26 20:48:19 +0900

    cirun/obsexec.py: Retry to connect to websocket server
    Just after starting obs, the websocket connection sometimes failed.
    Suspecting obs-websocket has not been started yet.

--- cirun/obsexec.py
+++ cirun/obsexec.py
@@ -64,4 +64,15 @@ class OBSExec:
     def get_obsws(self):
         global_cfg = self.config.get_global()
         obsws_pw = global_cfg['OBSWebSocket']['ServerPassword']
-        return obsws_python.ReqClient(host='localhost', port=4455, password=obsws_pw)
+        n_retry = 5
+        err = None
+        while n_retry > 0:
+            try:
+                return obsws_python.ReqClient(host='localhost', port=4455, password=obsws_pw)
+            except ConnectionRefusedError as e:
+                err = e
+                if self.proc_obs.poll() == None:
+                    sleep(1)
+                    print(f'Info: Failed to connect to obs-websocket {e=}. Retrying...')
+            n_retry -= 1
+        raise err
