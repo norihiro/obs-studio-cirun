559c78ec2eb31f971eef2d8b111eb819f1506e2e
Norihiro Kamae <norihiro@nagater.net>
2023-05-27 17:44:45 +0900

    cirun/test_projector.py: Test projectors

---
+++ cirun/test_projector.py
@@ -0,0 +1,109 @@
+import os
+import random
+import sys
+from time import sleep
+import unittest
+import pyautogui
+import obstest
+import util
+
+def _create_scene_with_color(cl, scene_name, source_name, color):
+    if not source_name:
+        source_name = 'color-' + scene_name
+    cl.send('CreateScene', {'sceneName': scene_name})
+    cl.send('CreateInput', {
+        'inputName': source_name,
+        'sceneName': scene_name,
+        'inputKind': 'color_source_v3',
+        'inputSettings': {
+            'color': color,
+            'width': 640,
+            'height': 360,
+        },
+    })
+
+
+class OBSProjectorTest(obstest.OBSTest):
+    def setUp(self):
+        super().setUp(run=False)
+
+    def test_open_projectors(self):
+        util.set_screenshot_prefix('screenshot/test_open_projectors-')
+
+        self.obs.config.get_global()['BasicWindow']['TransitionOnDoubleClick'] = 'true'
+        self.obs.config.save_global()
+
+        self.obs.run()
+
+        cl = self.obs.get_obsws()
+
+        cl.set_studio_mode_enabled(True)
+
+        for i in range(0, 16):
+            _create_scene_with_color(cl, f'Scene {i}', None, int(random.uniform(0xFF000000, 0xFFFFFFFF)))
+
+        cl.send('SetCurrentPreviewScene', {'sceneName': 'Scene 0'})
+        cl.send('SetCurrentProgramScene', {'sceneName': 'Scene 1'})
+
+        with self.subTest(msg='source'):
+            cl.send('OpenSourceProjector', {'sourceName': 'Scene 2'})
+            util.take_screenshot()
+            self._close_projector()
+
+        with self.subTest(msg='program'):
+            cl.send('OpenVideoMixProjector', {'videoMixType': 'OBS_WEBSOCKET_VIDEO_MIX_TYPE_PROGRAM'})
+            util.take_screenshot()
+            self._close_projector()
+
+        with self.subTest(msg='preview'):
+            cl.send('OpenVideoMixProjector', {'videoMixType': 'OBS_WEBSOCKET_VIDEO_MIX_TYPE_PREVIEW'})
+            util.take_screenshot()
+            self._close_projector()
+
+        with self.subTest(msg='multiview'):
+            cl.send('OpenVideoMixProjector', {'videoMixType': 'OBS_WEBSOCKET_VIDEO_MIX_TYPE_MULTIVIEW', 'monitorIndex': 0})
+            util.take_screenshot()
+
+            for i in range(8):
+                self._click_random(method=pyautogui.click)
+                util.take_screenshot()
+                pyautogui.doubleClick()
+                util.take_screenshot()
+
+        with self.subTest(msg='always-on-top'):
+            util.take_screenshot()
+            pyautogui.rightClick()
+            sleep(0.2)
+            pyautogui.hotkey('up')
+            pyautogui.hotkey('up')
+            sleep(1.0)
+            pyautogui.hotkey('enter')
+
+        with self.subTest(msg='close'):
+            pyautogui.rightClick()
+            sleep(0.5)
+            pyautogui.hotkey('up')
+            sleep(0.5)
+            pyautogui.hotkey('enter')
+            util.take_screenshot()
+
+    def _close_projector(self):
+        if sys.platform == 'darwin':
+            sleep(1)
+            pyautogui.hotkey('command', 'w')
+        else:
+            sleep(1)
+            pyautogui.rightClick(util.current_window_center())
+            sleep(0.5)
+            pyautogui.hotkey('up')
+            sleep(0.5)
+            pyautogui.hotkey('enter')
+
+    def _click_random(self, method):
+        cx = int(random.uniform(4, util.u.screenshot.width - 4))
+        cy = int(random.uniform(util.u.screenshot.height / 2, util.u.screenshot.height - 4))
+        method((cx, cy))
+
+
+if __name__ == '__main__':
+    unittest.main()
