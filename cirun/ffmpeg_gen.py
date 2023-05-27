80244face44c2c0486b7141f40e7675098f7ba45
Norihiro Kamae <norihiro@nagater.net>
2023-05-27 15:48:55 +0900

    Cache generated test source files
    Some test source files are generated by FFmpeg.
    However, it takes time to install ffmpeg on macOS. Cache the files on
    Linux and restore the cached files on macOS at the next run.
    This is not a right usage of the cache but still better than skipping
    test cases.

--- cirun/ffmpeg_gen.py
+++ cirun/ffmpeg_gen.py
@@ -6,6 +6,10 @@ import subprocess
 import os
 
 
+default_ext = 'flv'
+cache_path = 'ffmpeg_gen-cache/'
+
+
 _has_ffmpeg = False
 try:
     _p = subprocess.Popen(['ffmpeg', '-loglevel', 'error', '-version'])
@@ -13,9 +17,6 @@ try:
         _has_ffmpeg = True
 except:
     pass
-
-
-default_ext='flv'
 
 
 def _is_exist(fname):
@@ -29,12 +30,14 @@ def _is_exist(fname):
 def lavfi_testsrc2(ext=None):
     if not ext:
         ext = default_ext
-    fname = os.path.abspath('lavfi_testsrc2.' + ext)
+    fname = os.path.abspath(cache_path + 'lavfi_testsrc2.' + ext)
     if _is_exist(fname):
         return fname
 
     if not _has_ffmpeg:
         return None
+
+    os.makedirs(cache_path, exist_ok=True)
 
     p = subprocess.Popen([
         'ffmpeg',
