2a23e27a67c45c22c0dc629b5dad78a2e9ed2e4e
Norihiro Kamae <norihiro@nagater.net>
2023-05-27 17:42:55 +0900

    OBSTest: Add option to configure obs without starting

--- cirun/obstest.py
+++ cirun/obstest.py
@@ -16,16 +16,17 @@ def _is_obs_running():
 
 
 class OBSTest(unittest.TestCase):
-    def setUp(self, config_name='obs-config-default'):
+    def setUp(self, config_name='obs-config-default', run=True):
         if _is_obs_running():
             raise Exception('OBS has already been running')
-        self.obs = OBSExec(OBSConfigCopyFromSaved(config_name))
-        if sys.platform == 'linux':
-            util.wait_text('File Edit View Dock Profile Scene Collection Tools Help', timeout=10,
-                           ocrfunc=lambda u: util.ocr_topwindow(mode='top', length=200))
-        elif sys.platform == 'darwin':
-            util.wait_text('OBS Studio File Edit View Dock Profile Scene Collection Tools Help', timeout=10,
-                           ocrfunc=lambda u: u.ocr(crop=(0, 0, u.screenshot.width, 40)) )
+        self.obs = OBSExec(OBSConfigCopyFromSaved(config_name), run=run)
+        if run:
+            if sys.platform == 'linux':
+                util.wait_text('File Edit View Dock Profile Scene Collection Tools Help', timeout=10,
+                               ocrfunc=lambda u: util.ocr_topwindow(mode='top', length=200))
+            elif sys.platform == 'darwin':
+                util.wait_text('OBS Studio File Edit View Dock Profile Scene Collection Tools Help', timeout=10,
+                               ocrfunc=lambda u: u.ocr(crop=(0, 0, u.screenshot.width, 40)) )
 
     def tearDown(self):
         self.obs.term()
