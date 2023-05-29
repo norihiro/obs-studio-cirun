46c87b5ee35adcf1e31e03983caf39aa77d41455
Norihiro Kamae <norihiro@nagater.net>
2023-05-29 21:36:16 +0900

    cirun/test_plugins.py: Enable testing on Linux
    This commit also add these modifications.
    - Assertion by log file
    - Combine separated test methods into one test method
    
    Since 'Desktop Audio' is not available on Ubuntu on docker, use the
    obs-asynchronous-audio-source to test some audio filters.

--- cirun/test_plugins.py
+++ cirun/test_plugins.py
@@ -1,44 +1,40 @@
 import sys
 from time import sleep
+import re
 import unittest
 import obstest
 import obsplugin
+
+
+def _find_line_re(lines, cond):
+    if isinstance(cond, str):
+        cond = re.compile(cond)
+
+    for l in lines:
+        if cond.search(l):
+            return True
+    return False
 
 
 class OBSPluginTest(obstest.OBSTest):
     def setUp(self):
         super().setUp(run=False)
 
-    def test_async_audio_filter(self):
-        obsplugin.download_install_plugin_macos('norihiro/obs-async-audio-filter')
+    def tearDown(self):
+        super().tearDown()
+        obsplugin.uninstall_all_plugins()
 
-        self.obs.run()
-        cl = self.obs.get_obsws()
+    def _get_loglines(self):
+        with open(self.obs.get_logfile()) as f:
+            return f.read().split('\n')
 
-        cl.send('CreateSourceFilter', {
-            'sourceName': 'Desktop Audio',
-            'filterName': 'async-audio',
-            'filterKind': 'net.nagater.obs.' + 'asynchronous-audio-filter'
-        })
+    def _assert_log(self, loglines, cond):
+        self.assertTrue(_find_line_re(loglines, cond))
 
-        sleep(15)
-
-    def test_mute_filter(self):
-        obsplugin.download_install_plugin_macos('norihiro/obs-mute-filter')
-
-        self.obs.run()
-        cl = self.obs.get_obsws()
-
-        cl.send('CreateSourceFilter', {
-            'sourceName': 'Desktop Audio',
-            'filterName': 'mute-audio',
-            'filterKind': 'net.nagater.obs-mute-filter.' + 'mute-filter'
-        })
-
-        sleep(1)
-
-    def test_async_audio_source(self):
-        obsplugin.download_install_plugin_macos('norihiro/obs-asynchronous-audio-source')
+    def test_audio_plugins(self):
+        obsplugin.download_install_plugin('norihiro/obs-asynchronous-audio-source')
+        obsplugin.download_install_plugin('norihiro/obs-async-audio-filter')
+        obsplugin.download_install_plugin('norihiro/obs-mute-filter')
 
         self.obs.run()
         cl = self.obs.get_obsws()
@@ -53,13 +49,26 @@ class OBSPluginTest(obstest.OBSTest):
             },
         })
 
+        cl.send('CreateSourceFilter', {
+            'sourceName': name,
+            'filterName': 'async-audio',
+            'filterKind': 'net.nagater.obs.' + 'asynchronous-audio-filter'
+        })
+        sleep(15)
+
+        cl.send('CreateSourceFilter', {
+            'sourceName': name,
+            'filterName': 'mute-audio',
+            'filterKind': 'net.nagater.obs-mute-filter.' + 'mute-filter'
+        })
         sleep(1)
 
-
-# So far, This test works for macOS.
-# TODO: Also implement for Ubuntu 22.04.
-if sys.platform != 'darwin':
-    del OBSPluginTest
+        self.obs.term()
+        log = self._get_loglines()
+        self._assert_log(log, '\[obs-async-audio-filter\].*plugin loaded')
+        self._assert_log(log, '\[obs-mute-filter\].*plugin loaded')
+        self._assert_log(log, '\[obs-asynchronous-audio-source\].*plugin loaded')
+        self._assert_log(log, 'Number of memory leaks: 0$')
 
 
 if __name__ == '__main__':
