fd8b7cef75d9b4fd472d531d937cd39ab7269ef2
Norihiro Kamae <norihiro@nagater.net>
2023-06-02 17:38:24 +0900

    Test obs-color-monitor

--- cirun/test_plugins.py
+++ cirun/test_plugins.py
@@ -2,8 +2,11 @@ import sys
 from time import sleep
 import re
 import unittest
+import pyautogui
 import obstest
 import obsplugin
+import util
+import ffmpeg_gen
 
 
 def _find_line_re(lines, cond):
@@ -14,6 +17,25 @@ def _find_line_re(lines, cond):
         if cond.search(l):
             return True
     return False
+
+
+def _create_scene_with_media(cl, scene_name, source_name):
+    fname = ffmpeg_gen.lavfi_testsrc2()
+    if not fname:
+        return None
+    if not source_name:
+        source_name = 'media-' + scene_name
+    cl.send('CreateScene', {'sceneName': scene_name})
+    cl.send('CreateInput', {
+        'inputName': source_name,
+        'sceneName': scene_name,
+        'inputKind': 'ffmpeg_source',
+        'inputSettings': {
+            'local_file': ffmpeg_gen.lavfi_testsrc2(),
+            'looping': True,
+        },
+    })
+    return True
 
 
 class OBSPluginTest(obstest.OBSTest):
@@ -69,6 +91,59 @@ class OBSPluginTest(obstest.OBSTest):
         self._assert_log(log, '\[obs-mute-filter\].*plugin loaded')
         self._assert_log(log, '\[obs-asynchronous-audio-source\].*plugin loaded')
         self._assert_log(log, 'Number of memory leaks: 0$')
+
+    def test_color_monitor(self):
+        util.set_screenshot_prefix('screenshot/test_color_monitor-')
+        obsplugin.download_install_plugin('norihiro/obs-color-monitor')
+
+        self.obs.run()
+        cl = self.obs.get_obsws()
+
+        u = util.u
+        util.take_screenshot()
+        util.ocr_mainmenu()
+        t = util.click_verbose(u.find_text('Tools'))
+        util.ocr_verbose(crop=util.expand_locator(t, 270))
+        try:
+            util.click_verbose(u.find_text('New Scope Dock...'))
+        except:
+            # If not found, click the last item, assuming there are no other plugins.
+            pyautogui.hotkey('up')
+            pyautogui.hotkey('enter')
+        util.take_screenshot()
+        t = u.find_text('Other sources can be selected from the property after creating the dock.')
+        util.ocr_verbose(crop=util.expand_locator(t, 70))
+        if sys.platform == 'darwin':
+            try:
+                util.click_verbose(u.find_text('Cancel OK'), location_ratio=(1.0, 0.5))
+            except:
+                # if not found
+                util.click_verbose(u.find_text('Other sources can be selected from the property after creating the dock.'))
+                pyautogui.hotkey('enter')
+        else:
+            util.click_verbose(u.find_text('OK Cancel'), location_ratio=(0.0, 0.5))
+
+        _create_scene_with_media(cl, 'Scene 1', 'media-source')
+        cl.send('SetCurrentProgramScene', {'sceneName': 'Scene 1'})
+
+        sleep(5)
+        util.take_screenshot()
+
+        with self.subTest(msg='exit'):
+            self.obs.term()
+            log = self._get_loglines()
+            self._assert_log(log, 'Number of memory leaks: 0$')
+            self.assertFalse(obstest._is_obs_running())
+
+        with self.subTest(msg='run again'):
+            self.obs.run()
+            sleep(5)
+            self.assertTrue(obstest._is_obs_running())
+            util.take_screenshot()
+            self.obs.term()
+            log = self._get_loglines()
+            self._assert_log(log, 'Number of memory leaks: 0$')
+            self.assertFalse(obstest._is_obs_running())
 
 
 if __name__ == '__main__':
