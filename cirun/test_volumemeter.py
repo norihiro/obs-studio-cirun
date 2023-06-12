4ce4ad5b6086181f98af8087167ec94229ab9f8b
Norihiro Kamae <norihiro@nagater.net>
2023-06-12 22:08:54 +0900

    cirun/test_volumemeter.py (Windows): Add a audio source

--- cirun/test_volumemeter.py
+++ cirun/test_volumemeter.py
@@ -1,6 +1,8 @@
+import sys
 from time import sleep
 import unittest
 import obstest
+import obsplugin
 import util
 
 
@@ -9,10 +11,23 @@ class OBSStatsTest(obstest.OBSTest):
         super().setUp(run=False)
 
     def test_volmeter_vertical(self):
+        if sys.platform == 'win32':
+            obsplugin.download_install_plugin('norihiro/obs-asynchronous-audio-source')
         self.obs.config.get_global()['BasicWindow']['VerticalVolControl'] = 'true'
         self.obs.config.save_global()
 
         self.obs.run()
+
+        if sys.platform == 'win32':
+            cl = self.obs.get_obsws()
+            cl.send('CreateInput', {
+                'inputName': 'Sine wave',
+                'sceneName': 'Scene',
+                'inputKind': 'net.nagater.obs.' + 'asynchronous-audio-source',
+                'inputSettings': {
+                    'rate': 48000,
+                },
+            })
 
         sleep(2)
         util.take_screenshot()
