477d3e25307ae0beb554809fecd52258aaea45e6
Norihiro Kamae <norihiro@nagater.net>
2023-06-16 12:39:41 +0900

    cirun/test_ws.py: Add more test cases

--- cirun/test_ws.py
+++ cirun/test_ws.py
@@ -2,13 +2,14 @@ import json
 import os
 import random
 import sys
+import tempfile
 from time import sleep
 import unittest
 import pyautogui
 import obstest
 import util
 
-def req_batch(cl, requests):
+def req_batch(cl, requests, executionType=0):
     '''
     cl - obsws_python client, usually created by `self.obs.get_obsws()`
     requests - array of request, each request is a tuple of type and data.
@@ -18,7 +19,7 @@ def req_batch(cl, requests):
             'op': 8,
             'd': {
                 'requestId': str(random.uniform(0, 9999)),
-                'executionType': 0,
+                'executionType': executionType,
                 'requests': requests_data,
             },
     }
@@ -28,6 +29,7 @@ def req_batch(cl, requests):
 
 
 def _create_scene_with_color(cl, scene_name, source_name, color):
+    global next_executionType
     if not source_name:
         source_name = 'color-' + scene_name
     req_batch(cl, (
@@ -60,6 +62,14 @@ class OBSWSTest(obstest.OBSTest):
         with self.subTest(msg='GetSpecialInputs'):
             cl.send('GetSpecialInputs')
 
+        with self.subTest(msg='sources'):
+            cl.send('GetSourceActive', {'sourceName': 'color-random'})
+            cl.send('GetSourceScreenshot', {'sourceName': 'color-random', 'imageFormat': 'jpeg', 'imageWidth': 160, 'imageHeight': 90})
+            with tempfile.TemporaryDirectory() as td:
+                cl.send('SaveSourceScreenshot', {'sourceName': 'color-random', 'imageFormat': 'png', 'imageFilePath': str(td) + '/a.png', 'imageWidth': 160, 'imageHeight': 90})
+            cl.send('GetSourcePrivateSettings', {'sourceName': 'color-random'})
+            cl.send('SetSourcePrivateSettings', {'sourceName': 'color-random', 'sourceSettings': {}})
+
         with self.subTest(msg='RemoveInput'):
             _create_scene_with_color(cl, 'Scene Remove', 'color-remove',
                                      int(random.uniform(0xFF000000, 0xFFFFFFFF)))
@@ -89,6 +99,85 @@ class OBSWSTest(obstest.OBSTest):
             self.assertFalse(r.studio_mode_enabled)
 
             cl.send('GetMonitorList')
+
+        with self.subTest(msg='general'):
+            cl.send('GetVersion')
+            cl.send('GetStats')
+            cl.send('BroadcastCustomEvent', {'eventData': {'type': 'test_ws'}})
+            cl.send('GetHotkeyList')
+            cl.send('TriggerHotkeyByName', {'hotkeyName': 'OBSBasic.SplitFile'})
+            cl.send('TriggerHotkeyByKeySequence', {'keyId': 'OBS_KEY_A', 'keyModifiers': {'shift': True}})
+            cl.send('TriggerHotkeyByKeySequence', {'keyId': 'OBS_KEY_A', 'keyModifiers': {'control': True}})
+            cl.send('TriggerHotkeyByKeySequence', {'keyId': 'OBS_KEY_A', 'keyModifiers': {'alt': True}})
+            cl.send('TriggerHotkeyByKeySequence', {'keyId': 'OBS_KEY_A', 'keyModifiers': {'command': True}})
+
+        with self.subTest(msg='scenes'):
+            cl.send('GetGroupList')
+            cl.send('GetCurrentProgramScene')
+            cl.send('SetStudioModeEnabled', {'studioModeEnabled': True})
+            cl.send('GetCurrentPreviewScene')
+            cl.send('SetSceneName', {'sceneName': 'Scene', 'newSceneName': 'New Scene'})
+            cl.send('GetSceneItemList', {'sceneName': 'New Scene'})
+
+    def test_batch_types(self):
+        cl = self.obs.get_obsws()
+        with self.subTest(msg='batch-serialrealtime'):
+            req_batch(cl, (
+                ('CreateScene', {'sceneName': 'Scene 1'}),
+                ('Sleep', {'sleepMillis': 1}),
+                ('CreateInput', {
+                    'inputName': 'color 1',
+                    'sceneName': 'Scene 1',
+                    'inputKind': 'color_source_v3',
+                    'inputSettings': {
+                        'color': int(random.uniform(0xFF000000, 0xFFFFFFFF)),
+                        'width': 640,
+                        'height': 360,
+                    },
+                })), executionType=0)
+
+        with self.subTest(msg='batch-serialframe'):
+            req_batch(cl, (
+                ('CreateScene', {'sceneName': 'Scene 2'}),
+                ('Sleep', {'sleepFrames': 1}),
+                ('CreateInput', {
+                    'inputName': 'color 2',
+                    'sceneName': 'Scene 2',
+                    'inputKind': 'color_source_v3',
+                    'inputSettings': {
+                        'color': int(random.uniform(0xFF000000, 0xFFFFFFFF)),
+                        'width': 640,
+                        'height': 360,
+                    },
+                })), executionType=1)
+
+        sleep(1)
+
+        with self.subTest(msg='batch-parallel'):
+            req_batch(cl, (
+                ('RemoveInput', {'inputName': 'Scene 1'}),
+                ('RemoveInput', {'inputName': 'color 1'}),
+                ), executionType=2)
+        sleep(0.1)
+
+    def test_config(self):
+        cl = self.obs.get_obsws()
+
+        cl.send('SetPersistentData', {'realm': 'OBS_WEBSOCKET_DATA_REALM_GLOBAL', 'slotName': 'test_ws', 'slotValue': 128})
+        v = cl.send('GetPersistentData', {'realm': 'OBS_WEBSOCKET_DATA_REALM_GLOBAL', 'slotName': 'test_ws'}).slot_value
+        self.assertEqual(v, 128)
+
+        current_collection = cl.send('GetSceneCollectionList').current_scene_collection_name
+        cl.send('CreateSceneCollection', {'sceneCollectionName': 'new collection'})
+        cl.send('SetCurrentSceneCollection', {'sceneCollectionName': current_collection})
+
+        current_profile = cl.send('GetProfileList').current_profile_name
+        cl.send('CreateProfile', {'profileName': 'new profile'})
+        cl.send('GetVideoSettings')
+        cl.send('GetStreamServiceSettings')
+        cl.send('GetRecordDirectory')
+        cl.send('SetCurrentProfile', {'profileName': current_profile})
+        cl.send('RemoveProfile', {'profileName': 'new profile'})
 
 
 if __name__ == '__main__':
